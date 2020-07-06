50d9e19383c67e78c7f620caf9569d8681c682b3
YankeeWitNoBrim <adenusiprince221@gmail.com>
2020-07-06 21:05:26 +0000

    more stuff

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Substitute test', () =>{
+    it('Knows that racecar backwards is racecar', () =>{
+        expect(2 + 2).toEqual(4);
+    })
+})
